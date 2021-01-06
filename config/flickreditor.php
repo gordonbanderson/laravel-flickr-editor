3c71874e4d424711a638c79afa8528b3eed74887
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-01-06 17:24:58 +0000

    WIP: First cut of config

---
+++ config/flickreditor.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+
+    'flickrsets' => [
+        // 500 is the max allowed
+        'import_page_size' => env('FLICKR_SET_PAGE_IMPORT_SIZE', 500)
+    ],
+
+];
