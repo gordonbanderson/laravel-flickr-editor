a3896c4db23a57a924779e504905144794b4b3e7
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-01-13 17:57:25 +0000

    WIP: Editor

---
+++ resources/js/components/car.js
@@ -0,0 +1,7 @@
+class Car extends React.Component {
+    render() {
+        return <h2>I am a {this.props.color} Car!</h2>;
+    }
+}
+
+export default Car;
---
+++ resources/js/editor.js
@@ -0,0 +1,23 @@
+// Flickr editor
+import ShoppingListRender from "./components/shoppinglist";
+import Car from "./components/car";
+
+// needed for the browser to work
+import React from "react";
+import ReactDOM from 'react-dom';
+
+window.addEventListener('load', function() {
+    console.log('All assets are loaded')
+    // - Code to execute when all DOM content is loaded.
+    // - including fonts, images, etc.
+
+    alert('test');
+    console.log('Rendering components');
+//ReactDOM.render(<ShoppingListRender/>, document.getElementById('flickrEditor'));
+//ReactDOM.render(<Car color="red"/>, document.getElementById('root'));
+
+    var el = document.getElementById('root');
+    console.log('ELEMENT', el);
+    el.innerHTML('from js');
+});
+
