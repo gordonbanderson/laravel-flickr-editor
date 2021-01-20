adb7c9039517a4e0924038c9a8a969c65a482533
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-01-20 21:00:17 +0000

    FIX: Dynamic query now working

--- resources/flickr-edit-app/src/components/FlickrSet.js
+++ resources/flickr-edit-app/src/components/FlickrSet.js
@@ -4,24 +4,36 @@ import gql from 'graphql-tag';
 import {Link} from "react-router-dom";
 
 function FlickrSet(props) {
-    console.log(props);
+    console.log('PROPS', props);
 
-    const { loading, error, data } = useQuery(gql`
-        {
-          flickr_set(id: 1) {
+    let id=props.match.params.id;
+
+    /*
+    query ($limit: Int!) {
+          flickr_set(limit: $limit) {
+            id
             title
-            description
-            flickrPhotos {
-              title
-              description
-              small_url
-              small_width
-              small_height
-              id
-            }
           }
         }
-    `);
+     */
+    const { loading, error, data } = useQuery(gql`
+        query FlickrSet($id: Int!) {
+              flickr_set(id: $id) {
+                title
+                description
+                flickrPhotos {
+                  title
+                  description
+                  small_url
+                  small_width
+                  small_height
+                  id
+                }
+              }
+            }
+    `, {
+        variables: { id: parseInt(id,10) },
+    });
 
 
     if (loading) return <p>Loading...</p>;
@@ -29,13 +41,25 @@ function FlickrSet(props) {
 
     console.log('Data', data.flickr_set);
 
-    return data.flickr_set.flickrPhotos.map(({ title, id, small_url, small_height }) => (
-        <ul className={"ml-auto flex items-center"}>
-            <li className={"setPhoto"} key={id.toString()}>
+    /*
+    render() {
+    return (<div>
+    {this.state.people.map((person, index) => (
+        <p>Hello, {person.name} from {person.country}!</p>
+    ))}
+    </div>);
+}
+     */
+
+    return (<div className = "grid grid-cols-1 md:grid-cols-6" >
+        {data.flickr_set.flickrPhotos.map(({ title, id, small_url, small_height }) => (
+            <div className={"setPhoto"} key={id.toString()}>
                 <img src={small_url} title={title}/>
-            </li>
-        </ul>
-    ));
+            </div>
+        ))}
+    </div>)
+
+     ;
 }
 
 export default FlickrSet;
