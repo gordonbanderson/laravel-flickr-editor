4bbc710b732ea063f89b257e20f5ff0609259814
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-02-11 18:53:19 +0000

    FIX: Orphaned photos by date

--- src/GraphQL/Queries/PhotosByDate.php
+++ src/GraphQL/Queries/PhotosByDate.php
@@ -5,16 +5,18 @@ namespace Suilven\FlickrEditor\GraphQL\Queries;
 use Illuminate\Support\Facades\DB;
 use Suilven\FlickrEditor\Models\FlickrPhoto;
 
+// @TODO change to orphaned photos by date
 class PhotosByDate
 {
 
     public function __invoke($rootValue, array $args)
     {
-        // @todo Outer join
         return FlickrPhoto::orphanedOnDate($args['date'])
-            //->join('flickr_sets', 'flickr_photos.id', '=', 'flickr_photo_flickr_set.flickr_photo_id', 'full outer' )
+            ->whereNotIn('id', function ($query) {
+                //DB::table('flickr_photo_flickr_set')->select('flickr_photo_id')->get()->toArray())
+                $query->select('flickr_photo_id')->from('flickr_photo_flickr_set');
+            })
             ->orderBy('taken_at')
-
             ->get();
     }
 }
