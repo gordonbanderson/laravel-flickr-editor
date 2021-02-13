26c2d63fe55046d52cb9577c0f0603f9c7421b52
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-02-13 18:17:30 +0000

    FIX: Key bug

--- resources/flickr-edit-app/src/components/FlickrSets.js
+++ resources/flickr-edit-app/src/components/FlickrSets.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {GET_FLICKR_SET_LIST} from "../constants";
+import {GET_FLICKR_SET_LIST, GET_IMPORTED_FLICKR_SET_LIST} from "../constants";
 import {useQuery} from "@apollo/client";
 import {Link} from "react-router-dom";
 import {Helmet} from 'react-helmet';
 import {FLICKR_SET_SCREEN, getScreen, setScreen} from "./Screen";
 
 function FlickrSets(props)  {
-    const { loading, error, data } = useQuery(GET_FLICKR_SET_LIST);
+    const { loading, error, data } = useQuery(GET_IMPORTED_FLICKR_SET_LIST);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
@@ -19,14 +19,15 @@ function FlickrSets(props)  {
 
     return <div>
         <Helmet><title>Flickr Sets</title></Helmet>
-
-        {data.flickr_sets.map(({ title, id }) => (
         <ul>
-            <li key={id.toString()}>
+
+        {data.imported_flickr_sets.map(({ title, id, imported }) => (
+            <li key={id}>
                 <Link to={`/edit/set/`+id} onClick={setScreen(FLICKR_SET_SCREEN)}>Set {title}</Link>
             </li>
+        ))}
         </ul>
-        ))}</div>
+    </div>
     ;
 }
 
