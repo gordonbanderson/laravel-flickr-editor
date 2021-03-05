50c817e1b24f1674a0b5abad0d5411b4c86899ab
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-03-05 19:39:00 +0000

    FIx; tidy up of code as suggested by Matthew

--- resources/flickr-edit-app/src/components/HomePanel.js
+++ resources/flickr-edit-app/src/components/HomePanel.js
@@ -4,13 +4,14 @@ import {Helmet} from 'react-helmet';
 import IntroPage from "./IntroPage";
 
 export const HOME_TAB='Home';
-const EDIT_IMPORTED_TAB='Edit Imported Sets';
-const IMPORT_UNIMPORTED_TAB='Import Unimported Sets';
-const ORPHAN_TAB='Organise Orphan Photos';
-const STATUS_TAB='Status';
+export const EDIT_IMPORTED_TAB='Edit Imported Sets';
+export const IMPORT_UNIMPORTED_TAB='Import Unimported Sets';
+export const ORPHAN_TAB='Organise Orphan Photos';
+export const STATUS_TAB='Status';
+
 
 const Tab = props => {
-    const {selectedTab, setSelectedTab, children} = props;
+    const {selectedTab, setSelectedTab} = props;
     console.log('Tab props', props);
     return <button className={"tab" + (props.name === selectedTab ? ' active' : '')} onClick={() => {setSelectedTab(props.name);}}>
         <Link to={props.link}>{props.name}</Link>
@@ -20,15 +21,16 @@ const Tab = props => {
 function HomePanel(props)  {
     // @todo get the relevant tab from the route
     const [selectedTab, setSelectedTab] = useState(HOME_TAB);
+    const tabSharedProps = { selectedTab, setSelectedTab };
 
     return <div>
         <Helmet><title>Flickr Editor</title></Helmet>
             <nav className={'flex flex-row sm:flex-row'}>
-                <Tab name={HOME_TAB} link={"/editor"} setSelectedTab={setSelectedTab} selectedTab={selectedTab} />
-                <Tab name={EDIT_IMPORTED_TAB} link={"/editor/edit/sets"} setSelectedTab={setSelectedTab}  selectedTab={selectedTab}/>
-                <Tab name={IMPORT_UNIMPORTED_TAB} link={"/editor/import/sets"} setSelectedTab={setSelectedTab}  selectedTab={selectedTab}/>
-                <Tab name={ORPHAN_TAB} link={"/editor/orphan/photos"} setSelectedTab={setSelectedTab}  selectedTab={selectedTab}/>
-                <Tab name={STATUS_TAB} link={"/editor/status"} setSelectedTab={setSelectedTab}  selectedTab={selectedTab}/>
+                <Tab name={HOME_TAB} link="/editor" {...tabSharedProps} />
+                <Tab name={EDIT_IMPORTED_TAB} link="/editor/edit/sets" {...tabSharedProps}/>
+                <Tab name={IMPORT_UNIMPORTED_TAB} link="/editor/import/sets" {...tabSharedProps}/>
+                <Tab name={ORPHAN_TAB} link="/editor/orphan/photos" {...tabSharedProps}/>
+                <Tab name={STATUS_TAB} link="/editor/status" {...tabSharedProps}/>
             </nav>
             <IntroPage selectedTab={selectedTab}/>
         </div>
